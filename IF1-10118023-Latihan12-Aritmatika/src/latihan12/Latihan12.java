8b1ebff51284f52fabeae3a07a07fa4151a2b92e
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:52:54 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan12-Aritmatika/src/latihan12/Latihan12.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan12;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Aritmatika
+ */
+
+public class Latihan12 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("b--   = " + (a--));
+        System.out.println("d++   = " + (d++));
+        System.out.println("++d   = " + (++d));
+    }
+    
+}
